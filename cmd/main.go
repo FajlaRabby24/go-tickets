94937c9009eac914355c61811a766746b568d8f4
FajlaRabby24 <fajlarabbyla24@gmail.com>
2026-06-22 00:04:58 +0600

    feat: implement user registration with validation and database connection

--- cmd/main.go
+++ cmd/main.go
@@ -3,17 +3,48 @@ package main
 import (
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type User struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	gorm.Model
+	Name     string `json:"name" validate:"required,min=3" gorm:"type:varchar(100);not null"`
+	Email    string `json:"email" validate:"required,email" gorm:"type:varchar(100);uniqueIndex;not null"`
+	Password string `json:"password" validate:"required,min=6" gorm:"type:varchar(100);not null"`
+}
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i any) error {
+	if err := cv.validator.Struct(i); err != nil {
+		return echo.ErrBadRequest.Wrap(err)
+	}
+
+	return nil
 }
 
 func main() {
+	dsn := "host=localhost user=postgres password=12345 dbname=gotickets port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		TranslateError: true,
+	})
+
+	if err != nil {
+		panic("failed to connect database")
+	} else {
+		println("connected to database")
+	}
+
+	db.AutoMigrate(&User{})
+
 	e := echo.New()
+	e.Validator = &CustomValidator{validator: validator.New()}
 
 	e.Use(middleware.RequestLogger())
 	e.Use(middleware.Recover())
@@ -23,12 +54,24 @@ func main() {
 	})
 
 	e.POST("/users", func(c *echo.Context) error {
-		u := new(User)
-		if err := c.Bind(u); err != nil {
-			return err
+		newUser := new(User)
+
+		// binding the user data
+		if err := c.Bind(newUser); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]any{"error": err.Error()})
 		}
 
-		return c.JSON(http.StatusCreated, u)
+		// validating the user data
+		if err := c.Validate(newUser); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]any{"error": err.Error()})
+		}
+
+		// save to db
+		result := db.Create(newUser)
+		if result.Error != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]any{"error": result.Error.Error()})
+		}
+		return c.JSON(http.StatusCreated, newUser)
 	})
 
 	if err := e.Start(":5000"); err != nil {
